e879836ba839de0602cd2911a8f457b88ddc00fe
WenHao2130 <wenhao2130@outlook.com>
2026-06-21 01:11:48 +0800

    misc: oplus_motor: Import oplus motor driver
    Change-Id: I161fbf7b7a482722b3eb24cc90fc9cda6739e615
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

---
+++ drivers/misc/oplus_motor/camera_motor_ic/oppo_lt3572.h
@@ -0,0 +1,48 @@
+/************************************************************************************
+** Copyright (C), 2008-2017, OPPO Mobile Comm Corp., Ltd
+** File: oppo_drv8834.h
+**
+** Description:
+**	Definitions for motor driver ic drv8834.
+**
+** Version: 1.0
+** Date created: 2018/01/14,20:27
+**
+** --------------------------- Revision History: -------------------------------------
+* <version>		<date>		<author>		<desc>
+**************************************************************************************/
+#ifndef __OPPO_LT3572_H__
+#define __OPPO_LT3572_H__
+
+#define	 RATIO_A 29 /*2.9*/
+
+#define ERROR_NO (-11)
+
+enum {
+	GPIO_MODE = 0,
+	HIGH_IMPEDANCE_MODE
+};
+enum DUTY_RATIO {
+	RATIO_0_25 = 0,/*up*/
+	RATIO_0_75	/*down*/
+};
+
+struct oppo_lt_chip {
+	struct device	*dev;
+	struct pwm_device *pwm_dev;
+	struct pinctrl *pctrl;
+	struct pinctrl_state *pwm_state;
+	struct pinctrl_state *shdn_state;
+	struct pinctrl_state *shdna_state;
+	struct pinctrl_state *shdnb_state;
+	unsigned int step_gpio;
+	unsigned int shdn_gpio;
+	unsigned int shdna_gpio;
+	unsigned int shdnb_gpio;
+	int motor_type;
+	int duty_ratio;
+	int ratio_a;
+};
+
+#endif /*__OPPO_LT3572_H__*/
+
