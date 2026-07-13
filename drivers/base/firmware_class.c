9ea2ae110f21414529e7090b68b2ba4274ca4db2
ltdq <cyzi7777@163.com>
2026-07-13 19:48:18 +0800

    base: Add request_firmware_select for fw update
    - For vooc & tp firmware update
    
    Change-Id: I15d2604a22b0a1c9ea94b6b1d18cf21b21ae49a7
    Signed-off-by: ltdq <cyzi7777@163.com>

--- drivers/base/firmware_class.c
+++ drivers/base/firmware_class.c
@@ -119,6 +119,7 @@ static inline long firmware_loading_timeout(void)
 #endif
 #define FW_OPT_NO_WARN	(1U << 3)
 #define FW_OPT_NOCACHE	(1U << 4)
+#define FW_OPT_COMPARE	(1U << 5)
 
 struct firmware_cache {
 	/* firmware_buf instance will be added into the below list */
@@ -318,7 +319,7 @@ static void fw_finish_direct_load(struct device *device,
 }
 
 static int
-fw_get_filesystem_firmware(struct device *device, struct firmware_buf *buf)
+fw_get_filesystem_firmware(struct device *device, struct firmware_buf *buf, unsigned int opt_flags)
 {
 	loff_t size;
 	int i, len;
@@ -326,6 +327,10 @@ fw_get_filesystem_firmware(struct device *device, struct firmware_buf *buf)
 	char *path;
 	enum kernel_read_file_id id = READING_FIRMWARE;
 	size_t msize = INT_MAX;
+
+	/* OPLUS firmware comparison must be serviced by the userspace helper. */
+	if (opt_flags & FW_OPT_COMPARE)
+		return rc;
 
 	/* Already populated data member means we're loading into a buffer */
 	if (buf->data) {
@@ -929,6 +934,7 @@ exit:
 static int _request_firmware_load(struct firmware_priv *fw_priv,
 				  unsigned int opt_flags, long timeout)
 {
+	char *compare_envp[] = { "FwUp=compare", NULL };
 	int retval = 0;
 	struct device *f_dev = &fw_priv->dev;
 	struct firmware_buf *buf = fw_priv->buf;
@@ -953,7 +959,11 @@ static int _request_firmware_load(struct firmware_priv *fw_priv,
 		buf->need_uevent = true;
 		dev_set_uevent_suppress(f_dev, false);
 		dev_dbg(f_dev, "firmware: requesting %s\n", buf->fw_id);
-		kobject_uevent(&fw_priv->dev.kobj, KOBJ_ADD);
+		if (opt_flags & FW_OPT_COMPARE)
+			kobject_uevent_env(&fw_priv->dev.kobj, KOBJ_CHANGE,
+					   compare_envp);
+		else
+			kobject_uevent(&fw_priv->dev.kobj, KOBJ_ADD);
 	} else {
 		timeout = MAX_JIFFY_OFFSET;
 	}
@@ -1176,7 +1186,7 @@ _request_firmware(const struct firmware **firmware_p, const char *name,
 		}
 	}
 
-	ret = fw_get_filesystem_firmware(device, fw->priv);
+	ret = fw_get_filesystem_firmware(device, fw->priv, opt_flags);
 	if (ret) {
 		if (!(opt_flags & FW_OPT_NO_WARN))
 			dev_dbg(device,
@@ -1238,6 +1248,20 @@ request_firmware(const struct firmware **firmware_p, const char *name,
 	return ret;
 }
 EXPORT_SYMBOL(request_firmware);
+
+int
+request_firmware_select(const struct firmware **firmware_p, const char *name,
+		 struct device *device)
+{
+	int ret;
+
+	__module_get(THIS_MODULE);
+	ret = _request_firmware(firmware_p, name, device, NULL, 0,
+				FW_OPT_UEVENT | FW_OPT_FALLBACK | FW_OPT_COMPARE);
+	module_put(THIS_MODULE);
+	return ret;
+}
+EXPORT_SYMBOL(request_firmware_select);
 
 /**
  * request_firmware_direct: - load firmware directly without usermode helper
